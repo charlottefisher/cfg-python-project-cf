7b4b388156bf2bcb358b5ddf96229cf583d2c1f2
Charlotte Fisher <charlotte.h.fisher@gmail.com>
2019-03-06 19:42:54 +0000

    workshop 4
    templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,7 +7,7 @@ def say_hello():
   return render_template("index.html")
 
 @app.route("/<name>")
-def say_hello_to(name):
+def say_hello_too(name):
     return f"Hello {name}"
 
 @app.route("/qwerty")
@@ -18,4 +18,17 @@ def qwerty():
 
 #debug=True meens updateing the app.py here and
 #saving it will automatically reload the webpage :)
+
+@app.route("/<name>")
+def say_hello_to(name):
+  return render_template("hello.html", user=name)
+
+@app.route("/feedback", methods=["POST"])
+def get_feedback():
+  # request.values is a dictionary holding any
+  # POST request data that's not already part of the URL
+  data = request.values
+
+  return render_template("feedback.html", form_data=data)
+
 app.run(debug=True)
